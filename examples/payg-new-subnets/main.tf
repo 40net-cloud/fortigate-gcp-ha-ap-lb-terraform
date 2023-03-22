21d25cdc992a8dd93adce0cbdc5e4d1bc3d09b69
Bartek Moczulski <bmoczulski@gmail.com>
2023-03-22 13:57:28 +0100

    fix order
    subnets are now properly ordered

--- examples/payg-new-subnets/main.tf
+++ examples/payg-new-subnets/main.tf
@@ -43,7 +43,7 @@ module "fgt_ha" {
     env   : "test"
   }
   frontends     = ["app1"]
-  subnets       = [ for sb in google_compute_subnetwork.demo : sb.name ]
+  subnets       = [ for key in var.network_names: google_compute_subnetwork.demo[ key ].name ]
 
   depends_on    = [
     google_compute_subnetwork.demo
